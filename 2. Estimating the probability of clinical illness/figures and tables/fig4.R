781fe4fefbd181513403ed42cc632fc2ffe433de
rossaman4 <91492818+rossaman4@users.noreply.github.com>
2025-09-29 12:08:49 +0200

    Update fig4.R

--- 2. Estimating the probability of clinical illness/figures and tables/fig4.R
+++ 2. Estimating the probability of clinical illness/figures and tables/fig4.R
@@ -41,27 +41,22 @@ A1_param1 <- tail(A1_param1, n=8)[,c("mean","X50.","X2.5.", "X97.5.")]
 
 
 
-
-# overall mean proportion - min and max
+# mean proportions in one place 
 prop.df<-cbind(B2_param2$mean, B1_param2$mean,A2_param2$mean,A1_param2$mean,
             B2_param1$mean,B1_param2$mean,A2_param1$mean,A1_param1$mean)
 # median can also be used
 # prop.df<-cbind(B2_param2$X50., B1_param2$X50., A2_param2$X50., A1_param2$X50.,
 #               B2_param1$X50., B1_param1$X50., A2_param1$X50., A1_param1$X50.)
-prop.min <- apply(prop.df, 1, FUN=min)
-prop.max <- apply(prop.df, 1, FUN=max)
 
 
 
 # 2.5th centile - min 
 proplb.df<-cbind(B2_param2$X2.5., B1_param2$X2.5.,A2_param2$X2.5., A1_param2$X2.5.,
                B2_param1$X2.5., B1_param1$X2.5., A2_param1$X2.5., A1_param1$X2.5.)
-proplb.min <- apply(proplb.df, 1, FUN=min)
 
 # 97.5th centile - max
 propub.df<-cbind(B2_param2$X97.5., B1_param2$X97.5.,A2_param2$X97.5., A1_param2$X97.5.,
                  B2_param1$X97.5., B1_param1$X97.5., A2_param1$X97.5., A1_param1$X97.5.)
-propub.max <- apply(propub.df, 1, FUN=max)
 
 
 
@@ -69,15 +64,6 @@ propub.max <- apply(propub.df, 1, FUN=max)
 
 roundLabel<-c("Jun06","Aug06","Oct06","Dec06","Feb07","Apr07","Jun07","Aug07")
 endInterval<-seq(8.5,16.5)
-
-
-
-# extend variables for plotting which will give the 2-month blocks for the intervals
-endIntervalb <- c(8.5, rep(seq(9.5,15.5,1),each=2), 16.5)
-propMinCLb <- rep(proplb.min, each=2)
-propMaxCLb <- rep(propub.max, each=2)
-propMinb <- rep(prop.min, each=2)
-propMaxb <- rep(prop.max, each=2)
 
 
 
@@ -91,11 +77,9 @@ par(mgp=c(1.5,0.3,0))
 # length of tick marks
 par(tcl=-0.2)
 
-plot(endIntervalb, propMaxCLb, ylim=c(0,1.0),type="n", ylab="proportion", xlab="", xaxt="n", yaxt="n")
+plot(interval, prop.df[,1], ylim=c(0,1.0), xlim=c(9,16), type="p", ylab="proportion", pch=16, col="black", xlab="", xaxt="n", las=1)
+segments(interval, proplb.df[,1], interval, propub.df[,1], lwd=1.4)
 axis(1,at=seq(9,16),label=roundLabel)
-axis(2,las=1)
-polygon(c(endIntervalb, rev(endIntervalb)), c(propMinCLb, rev(propMaxCLb)), border=NA, col="lightskyblue1")
-polygon(c(endIntervalb ,rev(endIntervalb)), c(propMinb, rev(propMaxb)), border=NA, col="cornflowerblue")
 
 
 
